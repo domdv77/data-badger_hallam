850495c8f60bd9d3342ccc939d42225aacce4046
Domenico De Vivo <37935554+domdv77@users.noreply.github.com>
2018-10-16 22:13:23 +0100

    functionality and clean up
    Added functionality to grab the submission date and associated id's of each user submission. Removed useless code and added better comments.

--- GFDomsAddon.php
+++ GFDomsAddon.php
@@ -14,10 +14,8 @@ class GFDomsAddOn extends GFAddOn{
 
 	private static $_instance = null;
 	
-	//protected $sname;
-	//protected $sdate;
-	protected $form_choice;
-	protected $range_choice;
+	protected $form_choices = "FORM";
+	protected $range_choice = "";
 	
 	public static function get_instance(){
 		if (self::$_instance == null){
@@ -28,50 +26,62 @@ class GFDomsAddOn extends GFAddOn{
 	
 	public function init(){
 			parent::init();
-			add_filter('gform_pre_submission_1', array($this, 'save_name'));
+			add_filter('gform_post_submission_1', array($this, 'save_name'));
 			add_filter('gform_confirmation_1', array($this, 'conf_msg'));
 		if ( is_admin() ) {
-			add_filter('gform_pre_submission_2', array($this, 'save_selection'));
+			//add_filter('gform_pre_render_2', 'populate_form');
+			add_filter('gform_post_submission_2', array($this, 'save_selection'));
 			add_filter('gform_confirmation_2', array($this, 'admin_conf_msg'));
 		}
 	}
-	
+
+//USER FORM
 	public function save_name( $form ){
-		//$this->sname = "";
-		//$this->sdate = "";
-		$all = [];
-		
+	/*
+	*	create array, store entry id, and timestamp (converted to unix) when form is submitted
+	*/
+		$all = [];		
 		$all_entries = GFAPI::get_entries(1);//form id 1
+		$time_ids = [];
 		
+		//get date created of each submission and convert to unix format
 		foreach($all_entries as $entry){
 			$all += $entry;
-			//echo $entry['date_created'] . "###";
-	
-	/*
-	* 	TO DO
-	*	create dictionary, store entry id, and timestamp (converted to unix) when form is submitted
-	*/
-		
-		}
-		//var_dump($all);
-		//var_dump($all_entries);
-		//var_dump($all['date_created']);
-		//die;		
-		//$this->sname = $_POST['input_1_3']; 
-		//$this->sname = $all['1.3'];
-		//$this->sdate = $_POST['input_7'];
-		//$this->sdate = $all['7'];
-		//echo strtotime($all['date_created']); //convert timestamp to unix		
-				
+			$rest = $entry['date_created'];
+			$y = strtotime($rest);
+			
+			//populate array with id's associated with each submission time
+			array_push($time_ids, $rest, $entry['id']);
+		}	
+		var_dump($time_ids);
 	}
 	
 	public function conf_msg($confirmation){
 		$confirmation = 'Thank you ' . $_POST['input_1_3'] . ' for your message<br>';
 		$confirmation .= 'submitted on ' . $_POST['input_7'];
-		//$confirmation = 'Thank you ' . $this->sname . ' for your message<br>';
-		//$confirmation .= 'submitted on ' . $this->sdate;
 		return $confirmation;
 	}
+	
+	
+//ADMIN FORM
+/*	function populate_form($form){
+		//get all forms
+		$forms = GFAPI::get_forms();
+		
+		//make a list of forms and remove the admin form
+		$all_forms = [];
+		foreach ($forms as $form){
+			array_push($all_forms, $form['title']);
+		}
+		$key = array_search('Admin_Form', $all_forms);
+		unset($all_forms[$key]);
+		var_dump($all_forms);
+		
+		//pass list data to drop down option in the form
+
+	}
+*/
+	
 	
 	public function plugin_page(){	
 		echo '<h1>Admin Manage Entries</h1>';
@@ -79,30 +89,8 @@ class GFDomsAddOn extends GFAddOn{
 	}
 	
 	public function save_selection(){
-		$this->form_choice = "";
-		$this->range_choice = "";
-		$all_selections = [];
-		$all_data = [];
-		$all_data = GFAPI::get_entries(2);#select form id to get entries from
 		
-		
-		foreach ($all_data as $i){
-			$all_selections +=$i;
-		}
-			//var_dump($all_selections);
-			//echo $all_selections['form_id'];
-			//var_dump($all_selections['1']);
-			$this->form_choice = $all_selections['1'];
-			$this->range_choice = $all_selections['4'];
-			
-		//get the field
-		///$field = GFFormsModel::get_field($form, 2);
-		//get the html content
-		//$content = $field->content;
-		//var_dump($content);
-	
 	/*
-	* TO DO
 	*	search through dictionary to compare all entries older than user selected option
 	*	grab the id of all matching entries
 	*	grab form 1 entry id's to delete
@@ -112,13 +100,10 @@ class GFDomsAddOn extends GFAddOn{
 	
 	public function admin_conf_msg($admin_conf){
 		$admin_conf = '<br><br><h2>Entries deleted succesfully</h2>';
-		//$admin_conf .= '<br><br>You selected form ID: ' . $_POST['input_1'] . '<br>';
 		$admin_conf .= '<br><br>You selected form ID: ' . $_POST['input_1'] . '<br>';
 		if ($_POST['input_4'] == 'all'){
-			//$admin_conf .= 'and to delete ' . $this->range_choice . ' entries<br>';
 			$admin_conf .= 'and to delete ' . $_POST['input_4'] . ' entries<br>';
 		}else{
-			//$admin_conf .= 'and to delete all entries older than: ' . $this->range_choice;
 			$admin_conf .= 'and to delete all entries older than: ' . $_POST['input_4'];
 		}
 		return $admin_conf;
